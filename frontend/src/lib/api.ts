abbbe9817dd4d418fc1dd75a5de97f613b02256d
Developer <dev@specsheet.local>
2026-05-24 19:34:58 +0300

    fixed upload pdf from ui

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -10,6 +10,9 @@ const api: AxiosInstance = axios.create({
 });
 
 api.interceptors.request.use((config) => {
+  if (config.data instanceof FormData) {
+    delete config.headers["Content-Type"];
+  }
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
